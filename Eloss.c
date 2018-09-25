d76923b3764571c01d8bfb009cc3a1b04955fc10
ndzbenski <ndzbenski@hotmail.com>
2018-09-25 06:07:34 -0400

    Added Edep vs Theta plot

--- Eloss.c
+++ Eloss.c
@@ -42,7 +42,7 @@ void Eloss()
     //const int NEve = argv[1];
     const int NEve = 100000;
     
-    
+    const double PI = TMath::ACos(-1);
     
     
     //________________________________________________________________________________________________
@@ -53,9 +53,13 @@ void Eloss()
     
     TCanvas *c2 = new TCanvas("c2","energy loss v phi",800,600);
     
+    TCanvas *c3 = new TCanvas("c3","energy loss v theta",800,600);
+    
     TH1D *h_eLoss = new TH1D("h_eLoss","Energy loss",100,0,0);
     
-    TH2F *h_EvPhi = new TH2F("h_EvPhi","Eloss vs. Phi",100,0,15,100,0,365);
+    TH2F *h_Edep_v_phi = new TH2F("h_Edep_v_phi","Eloss vs. Phi",100,0,360,100,0,15);
+    
+    TH2F *h_Edep_v_theta = new TH2F("h_Edep_v_theta","Eloss vs. Theta",100,0,0,100,0,0);
     
     int n=0;
     
@@ -65,13 +69,14 @@ void Eloss()
     //________________________________________________________________________________________________
     
     // Copy file name
-    sprintf(filename,"/Users/nateMac/jlab_software/2.2/gemc/devel/gemc_Eloss.root");
+    sprintf(filename,"/Users/nateMac/jlab_software/2.2/gemc/devel/data/gemc_Eloss.root");
     //TFile *myfile= new TFile(filename, "open");
     
     //sprintf(filename,argv[0]);
     
     TFile *myfile= new TFile(filename, "open");
     TTree *tree= (TTree*)myfile->Get("rtpc");
+    TTree *gen_tree= (TTree*)myfile->Get("generated");
     
     std::vector<double>* CellID = 0;
     std::vector<double>* edep = 0;
@@ -79,10 +84,13 @@ void Eloss()
     std::vector<double>* Ypos = 0;
     std::vector<double>* Zpos = 0;
     
+    std::vector<double>* px = 0;
+    std::vector<double>* py = 0;
+    std::vector<double>* pz = 0;
+    
     TBranch *bcellID =  0;
     tree->SetBranchAddress("CellID",&CellID,&bcellID);
-    
-    // change after you get the name
+
     TBranch *bedep = 0;
     tree->SetBranchAddress("totEdep",&edep,&bedep);
     
@@ -95,8 +103,15 @@ void Eloss()
     TBranch *bZpos = 0;
     tree->SetBranchAddress("avg_z",&Zpos,&bZpos);
     
-    //TBranch *bpy = 0;
-    //tree->SetBranchAddress("py",&py,&bpy);
+    // generated info
+    TBranch *bpx = 0;
+    gen_tree->SetBranchAddress("px",&px,&bpx);
+    
+    TBranch *bpy = 0;
+    gen_tree->SetBranchAddress("py",&py,&bpy);
+    
+    TBranch *bpz = 0;
+    gen_tree->SetBranchAddress("pz",&pz,&bpz);
     
     
     //________________________________________________________________________________________________
@@ -108,6 +123,7 @@ void Eloss()
     for(int i=0; i<tree->GetEntries(); i++){
         
         Long64_t tentry = tree->LoadTree(i);
+        Long64_t gen_tentry = gen_tree->LoadTree(i);
         
         bcellID->GetEntry(tentry);
         bedep->GetEntry(tentry);
@@ -117,15 +133,21 @@ void Eloss()
         bYpos->GetEntry(tentry);
         bZpos->GetEntry(tentry);
         
-        double totEloss=0;
-        double phi_track=0;
+        // Get generated mom info
+        bpx->GetEntry(gen_tentry);
+        bpy->GetEntry(gen_tentry);
+        bpz->GetEntry(gen_tentry);
         
+        double totEloss=0;
+        
+        double p_tot = 0;
+        double phi_track = 0;
+        double theta_track = 0;
         
         // ________________________________________ Reconstruction _______________________________________
         
         // find postition from Cell ID
         for (UInt_t s = 0; s < CellID->size(); s++) {
-            
             
             // generated r position
             double r_pos=TMath::Sqrt(((Xpos->at(s))*(Xpos->at(s)))+((Ypos->at(s))*(Ypos->at(s))));
@@ -134,16 +156,18 @@ void Eloss()
             
             else totEloss += edep->at(s);
             
-            if (s == CellID->size()-1) {
-                phi_track = TMath::ATan2((Ypos->at(s)),(Xpos->at(s)))*(180/acos(-1))+180;
-                h_EvPhi->Fill(totEloss, phi_track);
-            }
-            
-            
         }// end finding position from Cell ID
         
+        
+        
+        p_tot = TMath::Sqrt((px->at(0)*px->at(0)) + (py->at(0)*py->at(0)));
+        phi_track = TMath::ATan2((py->at(0)),(px->at(0)))*(180/PI)+180.0;
+        theta_track = TMath::ATan2(p_tot,pz->at(0))*(180/PI);
+        
         if(totEloss != 0.0) h_eLoss->Fill(totEloss);
-
+        h_Edep_v_phi->Fill(phi_track, totEloss);
+        h_Edep_v_theta->Fill(theta_track, totEloss);
+        
     } // tree->GetEntries
     
     
@@ -162,14 +186,24 @@ void Eloss()
     c1->SaveAs("figs/Edep.png");
     // -------------------------------------------------------------------
     
-    // --------------------------- Eloss histogram ---------------------------
+    // --------------------------- Eloss vs Phi ---------------------------
     c2->cd();
-    h_EvPhi->Draw("CONT");
-    h_EvPhi->SetTitle("#phi v Edep");
-    //h_EvPhi->SetLineColor(kBlue);
-    h_EvPhi->GetXaxis()->SetTitle("Edep/track [MeV]");
-    h_EvPhi->GetYaxis()->SetTitle("#phi [deg]");
-    c2->SaveAs("figs/phi_v_Edep.png");
+    h_Edep_v_phi->Draw("CONT4");
+    h_Edep_v_phi->SetTitle("Edep v #phi");
+    //h_Edep_v_phi->SetLineColor(kBlue);
+    h_Edep_v_phi->GetXaxis()->SetTitle("#phi [deg]");
+    h_Edep_v_phi->GetYaxis()->SetTitle("Edep/track [MeV]");
+    c2->SaveAs("figs/Edep_v_phi.png");
+    // -------------------------------------------------------------------
+    
+    // --------------------------- Eloss vs Theta ---------------------------
+    c3->cd();
+    h_Edep_v_theta->Draw("CONT");
+    h_Edep_v_theta->SetTitle("Edep v #theta");
+    //h_Edep_v_theta->SetLineColor(kBlue);
+    h_Edep_v_theta->GetXaxis()->SetTitle("#theta [deg]");
+    h_Edep_v_theta->GetYaxis()->SetTitle("Edep/track [MeV]");
+    c3->SaveAs("figs/Edep_v_theta.png");
     // -------------------------------------------------------------------
     
     gPad->Update();
